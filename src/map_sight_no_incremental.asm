cbadcc42492232f2a4b9c93518a9b7cf1bfe2769
nguoiyoujie <yj.nguoi@yahoo.com>
2022-11-12 00:52:46 +0800

    Units revealing shroud will reveal the full radius instead of a ring around an outer 2 cell-range

--- src/map_sight_no_incremental.asm
+++ src/map_sight_no_incremental.asm
@@ -1,3 +1,8 @@
+; When a unit moves to remove shroud, the game filters out the inner range (sight - 2) to reduce the number of cells to process.
+; This assumes that the cells are already revealed.
+; With gap generators and shroud regrowth, this is often not the case
+; Therefore, we abandon the filter. Since the sight range is limited to ~10, the performance cost is peanuts compared to the rest of the code.
+
 @HOOK 0x004FE495 _MapClass_Sight_From_NoIncremental
 
 _MapClass_Sight_From_NoIncremental:
