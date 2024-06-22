df346435dd44e6af9bb8b7373df6fdfa87ebf9c9
nguoiyoujie <yj.nguoi@yahoo.com>
2024-06-22 11:06:26 +0800

    Improve build script to handle errors

--- build.cmd
+++ build.cmd
@@ -1,4 +1,29 @@
-copy ra95.dat ra95.exe
-tools\petool.exe ra95.exe edit AUTO rwxc
+@echo off
+@set TEMP=ra95_build.exe
+@set TARGET=ra95.exe
+del %TEMP% >nul 2>&1
+del %TARGET% >nul 2>&1
+copy ra95.dat %TEMP% 1>nul
+@if errorlevel 1 goto handleerror
+
+echo Building [petool]
+echo -------------------------------------------------------------
+tools\petool.exe %TEMP% edit AUTO rwxc
+@if errorlevel 1 goto handleerror
 ::tools\petool.exe ra95.exe add .patch rwxci 131072
-tools\linker.exe src\main.asm src\main.inc ra95.exe tools\nasm.exe -I./include/
+echo.
+echo Building [nasm]
+echo -------------------------------------------------------------
+tools\linker.exe src\main.asm src\main.inc %TEMP% tools\nasm.exe -I./include/
+@if errorlevel 1 goto handleerror
+@move %TEMP% %TARGET%
+echo.
+echo Success
+exit /b 0
+
+:handleerror
+echo.
+echo.
+echo An error has occured. Check above messages.
+pause
+exit /b 1
