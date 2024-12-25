be573ac3a59a6eaf80f1dabb9b84ebbd633f63b7
nguoiyoujie <yj.nguoi@yahoo.com>
2024-12-25 17:05:26 +0800

    Fill HouseClass offsets for future implementation

--- src/macros/houseclass.asm
+++ src/macros/houseclass.asm
@@ -21,83 +21,242 @@
 %define HouseClass.Offset.RepairDelay            0x018    ; word (Fixed)
 %define HouseClass.Offset.BuildDelay             0x01A    ; word (Fixed)
 
-; HouseStaticClass
-%define HouseStaticClass.Offset.IQ                     0x01C    ; INT
-%define HouseStaticClass.Offset.TechLevel              0x020    ; INT
-%define HouseStaticClass.Offset.Allies                 0x024    ; INT (Bitfield)
-%define HouseStaticClass.Offset.MaxUnit                0x028    ; INT
-%define HouseStaticClass.Offset.MaxBuilding            0x02C    ; INT
-%define HouseStaticClass.Offset.MaxInfantry            0x030    ; INT
-%define HouseStaticClass.Offset.MaxVessel              0x034    ; INT
-%define HouseStaticClass.Offset.MaxAircraft            0x038    ; INT
-%define HouseStaticClass.Offset.InitialCredits         0x03C    ; INT
-%define HouseStaticClass.Offset.Edge                   0x040    ; byte
+; HouseStaticClass Control;
+%define HouseClass.Offset.ControlIQ              0x01C    ; INT (there is a duplicate IQ further below)
+%define HouseClass.Offset.TechLevel              0x020    ; INT
+%define HouseClass.Offset.Allies                 0x024    ; INT (Bitfield)
+%define HouseClass.Offset.MaxUnit                0x028    ; INT
+%define HouseClass.Offset.MaxBuilding            0x02C    ; INT
+%define HouseClass.Offset.MaxInfantry            0x030    ; INT
+%define HouseClass.Offset.MaxVessel              0x034    ; INT
+%define HouseClass.Offset.MaxAircraft            0x038    ; INT
+%define HouseClass.Offset.InitialCredits         0x03C    ; INT
+%define HouseClass.Offset.Edge                   0x040    ; byte
+; /end HouseStaticClass Control;
 
 %define HouseClass.Offset.ActLike                0x041    ; byte (HousesType)
 
 %define HouseClass.Offset.IsActive               0x042    ; BOOL
 %define HouseClass.Bit.IsActive                  1    
 %define HouseClass.Offset.IsHuman                0x042    ; BOOL
-%define HouseClass.Bit.IsHuman                   2    
-%define HouseClass.Offset.WasHuman               0x042    ; BOOL
-%define HouseClass.Bit.WasHuman                  3    
+%define HouseClass.Bit.IsHuman                   2     
 %define HouseClass.Offset.IsPlayerControl        0x042    ; BOOL
-%define HouseClass.Bit.IsPlayerControl           4    
+%define HouseClass.Bit.IsPlayerControl           3    
 %define HouseClass.Offset.IsStarted              0x042    ; BOOL (Production)
-%define HouseClass.Bit.IsStarted                 5    
+%define HouseClass.Bit.IsStarted                 4    
 %define HouseClass.Offset.IsAlerted              0x042    ; BOOL
-%define HouseClass.Bit.IsAlerted                 6    
+%define HouseClass.Bit.IsAlerted                 5    
 %define HouseClass.Offset.IsBaseBuilding         0x042    ; BOOL
-%define HouseClass.Bit.IsBaseBuilding            7    
+%define HouseClass.Bit.IsBaseBuilding            6    
 %define HouseClass.Offset.IsDiscovered           0x042    ; BOOL
-%define HouseClass.Bit.IsDiscovered              8    
+%define HouseClass.Bit.IsDiscovered              7    
+%define HouseClass.Offset.IsMaxedOut             0x042    ; BOOL (Silos Needed)
+%define HouseClass.Bit.IsMaxedOut                8    
 
-%define HouseClass.Offset.IsMaxedOut             0x043    ; BOOL (Silos Needed)
-%define HouseClass.Bit.IsMaxedOut                1    
 %define HouseClass.Offset.IsDefeated             0x043    ; BOOL (Multiplay)
-%define HouseClass.Bit.IsDefeated                2    
+%define HouseClass.Bit.IsDefeated                1    
 %define HouseClass.Offset.IsToDie                0x043    ; BOOL 
-%define HouseClass.Bit.IsToDie                   3    
+%define HouseClass.Bit.IsToDie                   2    
 %define HouseClass.Offset.IsToWin                0x043    ; BOOL 
-%define HouseClass.Bit.IsToWin                   4    
+%define HouseClass.Bit.IsToWin                   3    
 %define HouseClass.Offset.IsToLose               0x043    ; BOOL 
-%define HouseClass.Bit.IsToLose                  5    
+%define HouseClass.Bit.IsToLose                  4    
 %define HouseClass.Offset.IsCivEvacuated         0x043    ; BOOL (One civ leaving the map in heli is enough)
-%define HouseClass.Bit.IsCivEvacuated            6    
+%define HouseClass.Bit.IsCivEvacuated            5    
 %define HouseClass.Offset.IsRecalcNeeded         0x043    ; BOOL (Recalc sidebar)
-%define HouseClass.Bit.IsRecalcNeeded            7    
+%define HouseClass.Bit.IsRecalcNeeded            6    
 %define HouseClass.Offset.IsVisionary            0x043    ; BOOL (Full map revealed)
-%define HouseClass.Bit.IsVisionary               8    
+%define HouseClass.Bit.IsVisionary               7    
+%define HouseClass.Offset.IsTiberiumShort        0x043    ; BOOL (Insufficient Funds)
+%define HouseClass.Bit.IsTiberiumShort           8    
 
-%define HouseClass.Offset.IsTiberiumShort        0x044    ; BOOL (Insufficient Funds)
-%define HouseClass.Bit.IsTiberiumShort           1    
 %define HouseClass.Offset.IsSpied                0x044    ; BOOL 
-%define HouseClass.Bit.IsSpied                   2    
+%define HouseClass.Bit.IsSpied                   1    
 %define HouseClass.Offset.IsThieved              0x044    ; BOOL 
-%define HouseClass.Bit.IsThieved                 3    
+%define HouseClass.Bit.IsThieved                 2    
 %define HouseClass.Offset.DidRepair              0x044    ; BOOL 
-%define HouseClass.Bit.DidRepair                 4    
+%define HouseClass.Bit.DidRepair                 3    
 %define HouseClass.Offset.IsGPSActive            0x044    ; BOOL 
-%define HouseClass.Bit.IsGPSActive               5    
+%define HouseClass.Bit.IsGPSActive               4    
 %define HouseClass.Offset.IsBuiltSomething       0x044    ; BOOL 
-%define HouseClass.Bit.IsBuiltSomething          6    
+%define HouseClass.Bit.IsBuiltSomething          5    
 %define HouseClass.Offset.IsResigner             0x044    ; BOOL 
-%define HouseClass.Bit.IsResigner                7    
+%define HouseClass.Bit.IsResigner                6    
 %define HouseClass.Offset.IsGiverUpper           0x044    ; BOOL 
-%define HouseClass.Bit.IsGiverUpper              8   
- 
-%define HouseClass.Offset.IsParanoid             0x045    ; BOOL 
-%define HouseClass.Bit.IsParanoid                1    
+%define HouseClass.Bit.IsGiverUpper              7   
+%define HouseClass.Offset.IsParanoid             0x044    ; BOOL 
+%define HouseClass.Bit.IsParanoid                8    
+
 %define HouseClass.Offset.IsToLook               0x045    ; BOOL 
-%define HouseClass.Bit.IsToLook                  2
+%define HouseClass.Bit.IsToLook                  1
  
 %define HouseClass.Offset.IQ                     0x046    ; INT
-%define HouseClass.Offset.State                  0x04A    ; INT
+%define HouseClass.Offset.State                  0x04A    ; byte
+
+%define HouseClass.Offset.SuperWeapon1           0x04B    ; 25 bytes
+;%define HouseClass.Offset.SuperWeapon1.Control        0x04E    ; CDTimerClass<FrameTimerClass>, 9 bytes
+;%define HouseClass.Offset.SuperWeapon1.OldStage       0x058    ; INT
+;%define HouseClass.Offset.SuperWeapon1.VoxRecharge    0x05C    ; byte
+;%define HouseClass.Offset.SuperWeapon1.VoxCharging    0x05D    ; byte
+;%define HouseClass.Offset.SuperWeapon1.VoxImpatient   0x05E    ; byte
+;%define HouseClass.Offset.SuperWeapon1.VoxSuspend     0x05F    ; byte
+;%define HouseClass.Offset.SuperWeapon1.Recharge       0x060    ; INT
+%define HouseClass.Offset.SuperWeapon2           0x064    ; 25 bytes
+%define HouseClass.Offset.SuperWeapon3           0x07D    ; 25 bytes
+%define HouseClass.Offset.SuperWeapon4           0x096    ; 25 bytes
+%define HouseClass.Offset.SuperWeapon5           0x0AF    ; 25 bytes
+%define HouseClass.Offset.SuperWeapon6           0x0C8    ; 25 bytes
+%define HouseClass.Offset.SuperWeapon7           0x0E1    ; 25 bytes
+%define HouseClass.Offset.SuperWeapon8           0x0FA    ; 25 bytes
+
+%define HouseClass.Offset.JustBuiltStructure     0x113    ; byte 
+%define HouseClass.Offset.JustBuiltInfantry      0x114    ; byte 
+%define HouseClass.Offset.JustBuiltUnit          0x115    ; byte 
+%define HouseClass.Offset.JustBuiltAircraft      0x116    ; byte 
+%define HouseClass.Offset.JustBuiltVessel        0x117    ; byte 
+
+%define HouseClass.Offset.Blockage               0x118    ; INT
+%define HouseClass.Offset.RepairTimer            0x11C    ; CDTimerClass<FrameTimerClass>, 9 bytes
+%define HouseClass.Offset.AlertTime              0x125    ; CDTimerClass<FrameTimerClass>, 9 bytes
+%define HouseClass.Offset.BorrowedTime           0x12E    ; CDTimerClass<FrameTimerClass>, 9 bytes
+
+%define HouseClass.Offset.BScan                  0x137    ; INT
+%define HouseClass.Offset.ActiveBScan            0x13B    ; INT
+%define HouseClass.Offset.OldBScan               0x13F    ; INT
+%define HouseClass.Offset.UScan                  0x143    ; INT
+%define HouseClass.Offset.ActiveUScan            0x147    ; INT
+%define HouseClass.Offset.OldUScan               0x14B    ; INT
+%define HouseClass.Offset.IScan                  0x14F    ; INT
+%define HouseClass.Offset.ActiveIScan            0x153    ; INT
+%define HouseClass.Offset.OldIScan               0x157    ; INT
+%define HouseClass.Offset.AScan                  0x15B    ; INT
+%define HouseClass.Offset.ActiveAScan            0x15F    ; INT
+%define HouseClass.Offset.OldAScan               0x163    ; INT
+%define HouseClass.Offset.VScan                  0x167    ; INT
+%define HouseClass.Offset.ActiveVScan            0x16B    ; INT
+%define HouseClass.Offset.OldVScan               0x16F    ; INT
+
+%define HouseClass.Offset.CreditsSpent           0x173    ; INT
+%define HouseClass.Offset.HarvestedCredits       0x177    ; INT
+%define HouseClass.Offset.StolenBuildingsCredits 0x17B    ; INT
+
+%define HouseClass.Offset.CurUnits               0x17F    ; INT
+%define HouseClass.Offset.CurBuildings           0x183    ; INT
+%define HouseClass.Offset.CurInfantry            0x187    ; INT
+%define HouseClass.Offset.CurVessels             0x18B    ; INT
+%define HouseClass.Offset.CurAircraft            0x18F    ; INT
+
+%define HouseClass.Offset.Tiberium               0x193    ; INT
+%define HouseClass.Offset.Credits                0x197    ; INT
+%define HouseClass.Offset.Capacity               0x19B    ; INT
+
+%define HouseClass.Offset.AircraftTotals         0x19F    ; INT PTR
+%define HouseClass.Offset.InfantryTotals         0x1A3    ; INT PTR
+%define HouseClass.Offset.UnitTotals             0x1A7    ; INT PTR
+%define HouseClass.Offset.BuildingTotals         0x1AB    ; INT PTR
+%define HouseClass.Offset.VesselTotals           0x1AF    ; INT PTR
+%define HouseClass.Offset.DestroyedAircraft      0x1B3    ; INT PTR
+%define HouseClass.Offset.DestroyedInfantry      0x1B7    ; INT PTR
+%define HouseClass.Offset.DestroyedUnits         0x1BB    ; INT PTR
+%define HouseClass.Offset.DestroyedBuildings     0x1BF    ; INT PTR
+%define HouseClass.Offset.DestroyedVessels       0x1C3    ; INT PTR
+%define HouseClass.Offset.CapturedBuildings      0x1C7    ; INT PTR
+%define HouseClass.Offset.TotalCrates            0x1CB    ; INT PTR
+
+%define HouseClass.Offset.AircraftFactories      0x1CF    ; INT PTR
+%define HouseClass.Offset.InfantryFactories      0x1D3    ; INT PTR
+%define HouseClass.Offset.UnitFactories          0x1D7    ; INT PTR
+%define HouseClass.Offset.VesselFactories        0x1DB    ; INT PTR
+%define HouseClass.Offset.BuildingFactories      0x1DF    ; INT PTR
+
+%define HouseClass.Offset.Power                  0x1E3    ; INT PTR
+%define HouseClass.Offset.Drain                  0x1E7    ; INT PTR
+
+%define HouseClass.Offset.AircraftFactory        0x1EB    ; INT PTR
+%define HouseClass.Offset.InfantryFactory        0x1EF    ; INT PTR
+%define HouseClass.Offset.UnitFactory            0x1F3    ; INT PTR
+%define HouseClass.Offset.VesselFactory          0x1F7    ; INT PTR
+%define HouseClass.Offset.BuildingFactory        0x1FB    ; INT PTR
+
+;%define HouseClass.Offset.Radar                  0x1FF    ; ? ; byte
+;%define HouseClass.Offset.FlagLocation           0x200    ; ? ; INT
+;%define HouseClass.Offset.FlagHome               0x204    ; ? ; short
+
+%define HouseClass.Offset.UnitsKilled            0x205    ; ? ; INT * 20 houses
+%define HouseClass.Offset.UnitsLost              0x255    ; ? ; INT
+%define HouseClass.Offset.BuildingsKilled        0x269    ; INT * 20 houses
+%define HouseClass.Offset.BuildingsLost          0x2A9    ; INT
+%define HouseClass.Offset.WhoLastHurtMe          0x2AD    ; byte (HousesType)
+
+;%define HouseClass.Offset.StartLocationOverride  0x2B2    ; INT ; remastered-only?
+
+%define HouseClass.Offset.Center                 0x2AE    ; INT
+%define HouseClass.Offset.Radius                 0x2B2    ; INT
+
+%define HouseClass.Offset.ZoneInfo               0x2B6    ; 5 zones * 12 bytes
+%define HouseClass.Offset.LATime                 0x2F2    ; INT
+%define HouseClass.Offset.LAType                 0x2F6    ; byte (RTTIType)
+%define HouseClass.Offset.LAType                 0x2F7    ; byte (ZoneType)
+%define HouseClass.Offset.LAEnemy                0x2F8    ; byte (HousesType)
+%define HouseClass.Offset.ToCapture              0x2F9    ; INT
+
+%define HouseClass.Offset.RadarSpied             0x2FD    ; INT
+%define HouseClass.Offset.PointTotal             0x301    ; INT
+%define HouseClass.Offset.PreferredTarget        0x305    ; byte
+
+%define HouseClass.Offset.BQuantity              0x306    ; 84 original count * INT
+%define HouseClass.Offset.UQuantity              0x456    ; 14 original count * INT
+%define HouseClass.Offset.IQuantity              0x48E    ; 24 original count * INT
+%define HouseClass.Offset.AQuantity              0x4EE    ; 7 original count * INT
+%define HouseClass.Offset.VQuantity              0x50A    ; 5 original count * INT
+
+%define HouseClass.Offset.Attack                 0x51E    ; CDTimerClass<FrameTimerClass>, 9 bytes
+%define HouseClass.Offset.Enemy                  0x527    ; byte (HousesType)
+%define HouseClass.Offset.AITimer                0x528    ; CDTimerClass<FrameTimerClass>, 9 bytes
+%define HouseClass.Offset.UnitToTeleport         0x531    ; INT
+
+%define HouseClass.Offset.BuildStructure         0x535    ; byte
+%define HouseClass.Offset.BuildUnit              0x536    ; byte
+%define HouseClass.Offset.BuildInfantry          0x537    ; byte
+%define HouseClass.Offset.BuildAircraft          0x538    ; byte
+%define HouseClass.Offset.BuildVessel            0x539    ; byte
+;0x53A
+%define HouseClass.Offset.Regions                0x53A    ; 4 bytes * 34 (w) * 34 (h)
+%define HouseClass.Offset.NukeDest               0x174A   ; word
+%define HouseClass.Offset.Allies                 0x174C   ; 4-byte bitfield
+%define HouseClass.Offset.DamageTime             0x1750   ; CDTimerClass<FrameTimerClass>, 9 bytes (looks like it is added into remaster)
+%define HouseClass.Offset.TeamTime               0x1759   ; CDTimerClass<FrameTimerClass>, 9 bytes (looks like it is added into remaster)
+%define HouseClass.Offset.TriggerTime            0x1762   ; CDTimerClass<FrameTimerClass>, 9 bytes (looks like it is added into remaster)
+%define HouseClass.Offset.SpeakAttackDelay       0x176B   ; CDTimerClass<FrameTimerClass>, 9 bytes (looks like it is added into remaster)
+%define HouseClass.Offset.SpeakPowerDelay        0x1774   ; CDTimerClass<FrameTimerClass>, 9 bytes (looks like it is added into remaster)
+%define HouseClass.Offset.SpeakMoneyDelay        0x177D   ; CDTimerClass<FrameTimerClass>, 9 bytes (looks like it is added into remaster)
+%define HouseClass.Offset.SpeakMaxedDelay        0x1786   ; CDTimerClass<FrameTimerClass>, 9 bytes (looks like it is added into remaster)
+
+%define HouseClass.Offset.RemapColor             0x178F   ; byte (PlayerColorType)
+%define HouseClass.Offset.IniName                0x1790   ; 12 bytes
+
+%define HouseClass.Offset.SaveLoadPadding        0x179C   ; Remastered: 256 bytes, this seems to be just 12 bytes
+; 0x17A8
+
+; Extended
+%define HouseClass.Offset.BPreGroupScan          0x17A8   ; INT (32 bit-field)
+; some empty space 0x17AC-0x17AF
+%define HouseClass.Offset.IsSpectator            0x17B0   ; byte (should be BOOL)
+%define HouseClass.Offset.SpawnLocation          0x17B4   ; word
+%define HouseClass.Offset.ConnectionLost         0x17B8   ; byte (should be BOOL)
+%define HouseClass.Offset.Resigned               0x17BC   ; byte (should be BOOL)
+%define HouseClass.Offset.InstantCapture         0x1800   ; byte (should be BOOL)
+%define HouseClass.Offset.NoBuildingCrew         0x1801   ; byte (should be BOOL)
+%define HouseClass.Offset.SecondaryColorScheme   0x1802   ; byte
+; some empty space 0x1803-0x180F
+%define HouseClass.Offset.NewBQuantity           0x1810   ; should be 256 * 4 bytes
+%define HouseClass.Offset.NewUQuantity           0x1C10   ; should be 256 * 4 bytes
+%define HouseClass.Offset.NewIQuantity           0x2010   ; should be 256 * 4 bytes
+%define HouseClass.Offset.NewAQuantity           0x2410   ; should be 256 * 4 bytes
+%define HouseClass.Offset.NewVQuantity           0x2810   ; should be 256 * 4 bytes
+
+
 ; building in progress
-
-
-;
-
 ; INI String controls
 ;str.TechnoTypeClass.IsDoubleOwned             db"DoubleOwned",0                 ;existing feature
 ;str.TechnoTypeClass.IsInvisible               db"Invisible",0                   ;existing feature
