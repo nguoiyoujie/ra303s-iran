d6fdaa2345d9f0be9a23d906db81073790d0f34a
nguoiyoujie <yj.nguoi@yahoo.com>
2025-02-06 19:01:21 +0800

    Include tool versions in build script

--- build.cmd
+++ build.cmd
@@ -4,9 +4,17 @@
 del %TEMP% >nul 2>&1
 del %TARGET% >nul 2>&1
 :: build
-echo Building [make]
+echo Listing tool versions
 echo -------------------------------------------------------------
 set PATH=C:\w64devkit\bin
+nasm -version
+echo.
+ld -version
+echo.
+make -version
+echo.
+echo Building [make]
+echo -------------------------------------------------------------
 make clean
 make
 @if errorlevel 1 goto handleerror
