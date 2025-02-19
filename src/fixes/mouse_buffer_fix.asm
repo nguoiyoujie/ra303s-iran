8ceb673f7e3f344d8c5d5946eb3bcce23279c0ba
nguoiyoujie <yj.nguoi@yahoo.com>
2025-02-20 06:51:34 +0800

    Fix: Use Frame check for main menu / select game dialogs

--- src/fixes/mouse_buffer_fix.asm
+++ src/fixes/mouse_buffer_fix.asm
@@ -7,8 +7,10 @@
 ; No compatibility issues is expected as this was not an adjustable parameter
 ;----------------------------------------------------------------
 
+cextern Globals___Frame
+
 ; test mouse crash fix?
-;@SJMP 0x005C17B0,0x005C17B7 ; WWMouseClass_WWMouseClass_SkipWINMM
+;@SJMP 0x005C17B0,0x005C17B7 ; WWMouseClass_WWMouseClass_SkipWINMM ; this skips hooking Process_Mouse() to external library
 
 ;@HACK 0x004A544D,0x004A5453,_GameOptionsClass__Run_ProcessMouse_Synchornously
 ;	jnz  .ProcessMouse
@@ -31,11 +33,11 @@
 
 
 @HACK 0x005C2184,0x005C218A,_WWMouseClass_Process_Mouse_SpecialDialog_Only
-	cmp  byte[0x00680834],0
+	cmp  byte[0x00680834],0 ; is there a special dialog in play?
 	jnz  .Continue
-    ; also check Session.Play
-	test byte[0x00680151],2
-    jnz  .Skip
+    ; also check Frame, (GameActive is 1 even on the main menu)
+	cmp  byte[Globals___Frame],0 ; Frame is 0 on the main menu
+    jne  .Skip
 .Continue:
 	mov  dword edx,[0x006AC284]
 	jmp  0x005C218A
