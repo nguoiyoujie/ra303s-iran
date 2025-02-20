a1f9b7b573d26ad78e109545d15a9a8de6ab0f53
nguoiyoujie <yj.nguoi@yahoo.com>
2025-02-20 17:08:02 +0800

    Enhancement: Capture weapons will capture based if damage would be dealt (i.e. 0% verses will be excluded). Also reset targetting and movement of captured units

--- src/features/warhead_capture.asm
+++ src/features/warhead_capture.asm
@@ -11,11 +11,37 @@
 ;----------------------------------------------------------------
 
 cextern Capture.OverrideIsCapturable
+cextern Modify_Damage
 
-@HACK 0x0051E0D9,0x0051E0E3,_ObjectClass__Take_Damage__CaptureCheck
-    ; ebx is the warhead
-    WarheadTypeClass.FromIndex(ecx,edx)
-    test byte[edx+WarheadTypeClass.Offset.IsCapture],1<<(WarheadTypeClass.Bit.IsCapture-1)
+@HACK 0x0051E103,0x0051E10D,_ObjectClass__Take_Damage__CaptureCheck
+    ; edx is the warhead
+    push edx
+    call Modify_Damage
+    pop  edx
+    ; eax is the damage amount
+    mov  [edi],eax
+    cmp  eax,0
+    jle  .Ret ; do nothing if the damage doesn't do anything
+    call CaptureCheck
+.Ret:
+    mov  edx,[ebp+0x8]
+    jmp  0x0051E10D
+@ENDHACK
+
+
+@HACK 0x0045AE0E,0x0045AE14,_BuildingClass__Captured__OverrideIsCapturable
+    jnz  0x0045AE14
+    test byte[Capture.OverrideIsCapturable],1
+    jz   0x0045B791
+    jmp  0x0045AE14
+@ENDHACK
+
+
+[section .text]
+CaptureCheck:
+    ; edx=warhead, esi=techno, ebp+0x8=source
+    WarheadTypeClass.FromIndex(edx,ebx)
+    test byte[ebx+WarheadTypeClass.Offset.IsCapture],1<<(WarheadTypeClass.Bit.IsCapture-1)
     jz   .Ret
     mov  edx,[ebp+0x8] ; source
     test edx,edx
@@ -30,15 +56,13 @@ cextern Capture.OverrideIsCapturable
     call dword[edi+0x1D4] ; TechnoClass::Captured
     mov  byte[Capture.OverrideIsCapturable],0
     pop  edi
+    ; if it is a capture, deal no damage    
+    mov  dword[edi],0
+    ; wipe the target's memory: NavCom and TarCom
+    test al,al
+    jz   .Ret
+    mov  dword[esi+TechnoClass.Offset.TarCom],-1
+    mov  dword[esi+FootClass.Offset.NavCom],-1
 .Ret:
-    cmp  dword[ebp+0xC],0
-    jmp  0x0051E0E3
-@ENDHACK
+    ret
 
-
-@HACK 0x0045AE0E,0x0045AE14,_BuildingClass__Captured__OverrideIsCapturable
-    jnz  0x0045AE14
-    test byte[Capture.OverrideIsCapturable],1
-    jz   0x0045B791
-    jmp  0x0045AE14
-@ENDHACK
